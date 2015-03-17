ae771e1d3a462c129352d6a79c9c516af79b760a
Tarik Hamilton <tarikhamilton@gmail.com>
2015-03-17 09:55:45 -0400

    Sorts in ascending order

--- match-column-height.js
+++ match-column-height.js
@@ -1,20 +1,16 @@
 var equalHeights = function (columns) {
 
-	function getHeight(c, i, a) {
-		return a[i].clientHeight;
-	}
-
 	var countOfColumns = [];
+	var heightsOfColumns = [];
 	var i = 0;
 
 	while (i < columns.length) {
 		countOfColumns.push(0);
 		i++;
 	} 
-	console.dir(countOfColumns);
-
-	countOfColumns.forEach.call(columns, getHeight);
-
-	
-
+	countOfColumns.forEach.call(columns, function (c, i, a) { 
+		heightsOfColumns.push([c, a[i].clientHeight]); 
+	});
+	heightsOfColumns.sort(function (a, b) { return a[1] - b[1] });
+	console.dir(heightsOfColumns);
 }
